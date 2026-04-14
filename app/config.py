fb7e70d0b6a9233aab45643f511844c920a8d63c
hoodev490 <hoodev490@gmail.com>
2026-04-14 18:59:39 -0400

    Add comment explaining SECRET_KEY usage

--- app/config.py
+++ app/config.py
@@ -2,6 +2,8 @@ import os
 
 
 class Config:
+    # Used by Flask to sign session cookies. Not actively used since we have no sessions,
+    # but kept as a default Flask convention in case sessions are added later.
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
     DEBUG = False
     MLB_API_BASE_URL = "https://statsapi.mlb.com"
